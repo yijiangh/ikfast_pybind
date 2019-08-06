f41734dce887d86fc27e02ae309b32dce30bcbd9
Yijiang Huang <yijiangh@mit.edu>
2019-08-06 21:05:58 +0200

    Add kuka_kr6_r900, abb_irb4600_40_255

---
+++ setup.py
@@ -0,0 +1,111 @@
+import os
+import re
+import sys
+import platform
+import subprocess
+import io
+
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+from distutils.version import LooseVersion
+
+
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=''):
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+
+class CMakeBuild(build_ext):
+    def run(self):
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError("CMake must be installed to build the following extensions: " +
+                               ", ".join(e.name for e in self.extensions))
+
+        if platform.system() == "Windows":
+            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
+            if cmake_version < '3.1.0':
+                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
+
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        cmake_args = ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
+                      '-DPYTHON_EXECUTABLE=' + sys.executable]
+
+        cfg = 'Debug' if self.debug else 'Release'
+        build_args = ['--config', cfg]
+
+        if platform.system() == "Windows":
+            cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
+            if sys.maxsize > 2**32:
+                cmake_args += ['-A', 'x64']
+            build_args += ['--', '/m']
+        else:
+            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+            build_args += ['--', '-j2']
+
+        env = os.environ.copy()
+        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get('CXXFLAGS', ''),
+                                                              self.distribution.get_version())
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+        subprocess.check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp, env=env)
+        subprocess.check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
+
+# this script is adapted from cmake_example project for pybind11
+# https://github.com/pybind/cmake_example
+
+# for more info for extension modules:
+# https://docs.python.org/2/distutils/setupscript.html
+
+ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*names, **kwargs):
+    return io.open(
+        os.path.join(ROOT_DIR, *names),
+        encoding=kwargs.get('encoding', 'utf8')
+    ).read()
+
+
+long_description = read('README.md')
+# requirements = read('requirements.txt').split('\n')
+# optional_requirements = {
+# }
+
+EXT_MODULES = [CMakeExtension('ikfast_kuka_kr6_r900'),
+               CMakeExtension('ikfast_abb_irb4600_40_255')]
+
+setup(
+    name='ikfast_pybind',
+    version='0.0.1',
+    author='Yijiang Huang',
+    author_email='yijiangh@mit.edu',
+    description='ikfast_pybind is a python binding generation library for the analytic kinematics engine ikfast.',
+    long_description=long_description,
+    url="https://github.com/yijiangh/ikfast_pybind",
+    # packages=['ikfast_pybind'],
+    # package_dir={'': 'src'},
+    ext_modules=EXT_MODULES,
+    cmdclass=dict(build_ext=CMakeBuild),
+    zip_safe=False,
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        'Operating System :: Unix',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Robotics',
+    ],
+    keywords=['Robotics', 'kinematics'],
+)
