24631d4ef7e8a212146089a597978229b337fd14
Yijiang Huang <yijiang94817@gmail.com>
2023-07-22 01:59:37 +0200

    enforce sympy version and add dae rounding procedure

--- Dockerfile
+++ Dockerfile
@@ -13,11 +13,20 @@
 # COPY src /src
 # ENTRYPOINT ["/entrypoint.bash"]
 
+#####################
+
 # https://answers.ros.org/question/263925/generating-an-ikfast-solution-for-4-dof-arm/?answer=265625#post-id-265625
 FROM personalrobotics/ros-openrave
 RUN apt-get update || true && apt-get install -y --no-install-recommends build-essential python-pip liblapack-dev && apt-get clean && rm -rf /var/lib/apt/lists/*
-RUN pip install sympy==0.7.1
+# enforce a specific version of sympy, which is known to work with OpenRave
+# https://github.com/ros-planning/moveit/pull/2650
+# RUN pip install sympy==0.7.1
+RUN pip install git+https://github.com/sympy/sympy.git@sympy-0.7.1
+RUN  apt-get -y update && \
+  apt install -y wget ros-indigo-collada-urdf
+RUN wget https://raw.githubusercontent.com/ros-planning/moveit/indigo-devel/moveit_kinematics/ikfast_kinematics_plugin/scripts/round_collada_numbers.py
 
 ENTRYPOINT ["/ikfast_pybind/entrypoint.bash"]
 
 # docker run -v ${PWD}:/ikfast_pybind personalrobotics/ros-openrave [base_link] [effector] [module_extension]
+# docker run -it --rm -v ${PWD}:/ikfast_pybind personalrobotics/ros-openrave
--- entrypoint.bash
+++ entrypoint.bash
@@ -7,10 +7,11 @@ EXTENSION=${4}
 
 # Generate inverse kinematics files
 cp /ikfast_pybind/data/${URDF_FILE} ${URDF_FILE}
-source /opt/ros/kinetic/local_setup.bash
+./ros_entrypoint.sh
 rosrun collada_urdf urdf_to_collada ${URDF_FILE} robot.dae
-cat <<EOT >> robot.xml
-<robot file="robot.dae">
+python round_collada_numbers.py robot.dae robot.rounded.dae 5
+cat <<EOT > robot.xml
+<robot file="robot.rounded.dae">
   <Manipulator name="robot_workspace">
     <base>${BASE}</base>
     <effector>${EFFECTOR}</effector>
@@ -19,28 +20,36 @@ cat <<EOT >> robot.xml
 EOT
 
 # [recommanded] let ikfast decides where to put the free joint
-openrave.py --database inversekinematics --robot=robot.xml --iktype=transform6d --iktests=100
+# clean up all previously generated ikfast files
+rm -rf ~/.openrave/*
+openrave0.9.py --database inversekinematics --robot=robot.xml --iktype=transform6d --iktests=100
 
 # [archived] specify the free joint
 # python `openrave-config --python-dir`/openravepy/_openravepy_/ikfast.py --robot=robot.dae --iktype=transform6d --baselink=1 --eelink=9  --freeindex=4 --savefile=ikfast*.Transform6D.*.cpp
 
-export NEW_IKMOD_DIR=/ikfast_pybind/src/${EXTENSION}
+NEW_IKMOD_DIR=/ikfast_pybind/src/${EXTENSION}
 
 cp -r /ikfast_pybind/src/_template ${NEW_IKMOD_DIR}
 
 # `ikfast` generated files
-cp $(find . -maxdepth 1 -name 'ikfast*.Transform6D.*.cpp') ${NEW_IKMOD_DIR}/ikfast_source.cpp
-cp $(find . -maxdepth 1 -name 'ikfast.h') ${NEW_IKMOD_DIR}/ikfast.h
+cp $(find ~/.openrave/ -name 'ikfast*.Transform6D.*.cpp') ${NEW_IKMOD_DIR}/ikfast_source.cpp
+cp $(find ~/.openrave/ -name 'ikfast.h') ${NEW_IKMOD_DIR}/ikfast.h
 
 sed -i "s/\[put_extension\]/${EXTENSION}/g" ${NEW_IKMOD_DIR}/CMakeLists.txt
 sed -i "s/\[put_extension\]/${EXTENSION}/g" ${NEW_IKMOD_DIR}/ikfast_pybind_wrapper.cpp
 sed -i "s/\[put_extension\]/${EXTENSION}/g" ${NEW_IKMOD_DIR}/test_[put_extension].py
 
+# comment out these two lines
+sed -i 's!#define IKFAST_COMPILE!// #define IKFAST_COMPILE!g' ${NEW_IKMOD_DIR}/ikfast_pybind_wrapper.cpp
+sed -i 's!IKFAST_COMPILE_ASSERT(IKFAST!// IKFAST_COMPILE_ASSERT(IKFAST!g' ${NEW_IKMOD_DIR}/ikfast_pybind_wrapper.cpp
+
 # move test file rename urdf for testing
 mv ${NEW_IKMOD_DIR}/test_[put_extension].py /ikfast_pybind/tests/test_${EXTENSION}.py
-mv /ikfast_pybind/data/${URDF_FILE} /ikfast_pybind/data/${EXTENSION}.urdf
+mv ${URDF_FILE} /ikfast_pybind/data/${EXTENSION}.urdf
 
 # append "# ikfast" to the end of the CMakeLists.txt
+# ? I still can't figure out how to put a linebreak before add_subdirectory
 cat <<EOT >> /ikfast_pybind/src/CMakeLists.txt
-\nadd_subdirectory(${EXTENSION})
+
+add_subdirectory(${EXTENSION})
 EOT
